1fb7b2681b29242e0369065fbf8b87e9295c0bf7
monwarqx1250 <monwarh.0.1@gmail.com>
2026-04-06 02:30:38 +0600

    Update PostCardMedia component

--- frontend/src/components/feed/PostCardMedia.jsx
+++ frontend/src/components/feed/PostCardMedia.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 
 export default function PostCardMedia({ title, image }) {
 	return (
-		<>
+		<div style={{padding:10}}>
 			<h4 className="_feed_inner_timeline_post_title">{title || 'Untitled Post'}</h4>
-			<div className="_feed_inner_timeline_image">
-				<img src={image || "assets/images/timeline_img.png"} alt="" className="_time_img" />
-			</div>
-		</>
+			{image &&
+				<div className="_feed_inner_timeline_image">
+					<img src={image} alt="" className="_time_img" />
+				</div>
+			}
+		</div>
 	);
 }
